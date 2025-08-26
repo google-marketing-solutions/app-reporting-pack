a13be592d80d242d5a4fe5ee72fa07ab807c7473
Andrei Markin <amarkin@google.com>
2025-08-26 16:15:37 +0400

    feat: add support for v21 API
    Change-Id: I43a1f4e15f57e464c1870a687099f2e02defa7a9

--- app/run-local.sh
+++ app/run-local.sh
@@ -47,8 +47,8 @@ solution_name="App Reporting Pack"
 solution_name_lowercase=$(echo $solution_name | tr '[:upper:]' '[:lower:]' |\
   tr ' ' '_')
 
-API_VERSION="20"
-supported_api_versions="18 19 20"
+API_VERSION="21"
+supported_api_versions="19 20 21"
 quiet="n"
 generate_config_only="n"
 validate_ads_config="n"
